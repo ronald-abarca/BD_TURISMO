8a3244a93dd795fdd0ebef68b5400c95feabf774
HenryGuzman <henry15ea@gmail.com>
2023-03-27 17:40:24 -0600

    - Eliminada la tabla cliente y se engloba en la tabla usuarios . - Se crea la tabla de roles que manejara el rol de la cuenta para la tabla usuarios . - Se agrego la tabla 'paqueteCalificacion' para almacenar las calificaciones que los usuarios le dan a los paquetes turisticos . - en la tabla adicionales , se agrego campo de nombre para identificar que elemento es y la descripcion del mismo. - cambios agregados en para obtener las fechas automaticamente .

--- BD_TURISMO.sql
+++ BD_TURISMO.sql
@@ -1,4 +1,5 @@
 create database BD_TURISMO
+
 go
 use BD_TURISMO
 go
@@ -11,23 +12,49 @@ create table municipios(
 	idmunicipio varchar(120) primary key,
 	nombre varchar(50),
 	iddepartamento varchar(120) foreign key references departamentos(iddepartamento)
-)
+);
+
+go 
+create table rolusuario(
+	id_role varchar(64) primary key,
+	r_name varchar(50) default 'USER',
+	r_registro DATETIME2 default CURRENT_TIMESTAMP
+);
+
 go
 create table usuarios(
 	idusuario varchar(120) primary key,
 	nombre varchar(100),
 	apellido varchar(100),
+	edad tinyint default 18,
+	telefono varchar(15) default '00-00-00-00',
+	direccion varchar(120) default 'col demno',
 	correo varchar(100),
-	contrasegna varchar(120),
-	fechacreacion datetime,
-	estado bit
+	id_rol varchar(64) not null,
+	fechacreacion DATETIME2 default CURRENT_TIMESTAMP,
+	estado bit,
+	constraint fk_rl foreign key (id_rol) references rolusuario(id_role)
 )
+go
+create table cuenta(
+	id_cuenta varchar(120) not null,
+	id_usuario varchar(120) not null,
+	u_name varchar(50) default 'usuario',
+	u_pass varchar(50) default '0x202CB962AC59075B964B07152D234B70',
+	u_state bit,
+	u_registro DATETIME2 default CURRENT_TIMESTAMP,
+	constraint pk_cuenta primary key(id_cuenta,id_usuario),
+	constraint fk_user foreign key (id_usuario) references usuarios(idusuario)
+);
+
 go
 create table telefono(
 	idtelefono varchar(120) primary key,
 	telefono varchar(20),
 	idusuario varchar(120) foreign key references usuarios(idusuario)
 )
+
+
 go
 create table categorias(
 	idcategoria varchar(120) primary key,
@@ -39,16 +66,29 @@ create table paquetes(
 	idpaquete varchar(120) primary key,
 	nombre varchar(100),
 	descripcion varchar(300),
+	direccion varchar(200),
 	idmunicipio varchar(120) foreign key references municipios(idmunicipio),
 	idcategoria varchar(120) foreign key references categorias(idcategoria),
 	img varchar(500)
 )
+
+go 
+
+create table paqueteCalificacion(
+	id_calificacion varchar(120) not null,
+	id_usuario varchar(120) not null foreign key references usuarios(idusuario),
+	id_paquete varchar(120) not null foreign key references paquetes(idpaquete),
+	nota bit default 0,
+	fecha DATETIME2 default CURRENT_TIMESTAMP,
+	constraint pk_cal primary key (id_calificacion,id_usuario)
+);
+
 go
 create table paquetesdisponible(
 	idpaqueted varchar(120) primary key,
 	idpaquete varchar(120) foreign key references paquetes(idpaquete),
 	precio money,
-	cupos int,
+	cupos_disp int,
 	cuposllenos int,
 	fechainicial datetime,
 	fechafinal datetime,
@@ -57,6 +97,7 @@ create table paquetesdisponible(
 go
 create table adicionales(
 	idadicional varchar(120) primary key,
+	nombre varchar(200) ,
 	descripcion varchar(100),
 	precio money,
 )
@@ -66,16 +107,7 @@ create table adicionalesdisponible(
 	idpaqueted varchar(120) foreign key references paquetesdisponible(idpaqueted),
 	idadicional varchar(120) foreign key references adicionales(idadicional)
 )
-go
-create table cliente(
-	idcliente varchar(120) primary key,
-	nombre varchar(100),
-	apellido varchar(100),
-	idtelefono varchar(120) foreign key references telefono(idtelefono),
-	correo varchar(100),
-	fechacreacion datetime,
-	estado bit
-)
+
 go
 create table formapago(
 	idformapago varchar(120) primary key,
@@ -86,11 +118,13 @@ create table formapago(
 go
 create table encabezado(
 	idencabezado varchar(120) primary key,
-	idcliente varchar(120) foreign key references cliente(idcliente),
+	idcuenta varchar(120) not null,
 	idformapago varchar(120) foreign key references formapago(idformapago),
 	descuento money,
 	monto money,
-	fecha datetime
+	fecha DATETIME2 default CURRENT_TIMESTAMP,
+
+	constraint fk_uen foreign key (idcuenta) references usuarios(idusuario)
 )
 go
 create table detalle(
@@ -107,17 +141,13 @@ create table personasextras(
 	idagregado varchar(120) primary key,
 	nombre varchar(100),
 	apellido varchar(100),
+	n_doc varchar(50),
+	edad tinyint default 18,
 	iddetalle varchar(120) foreign key references detalle(iddetalle),
-	idcliente varchar(120) foreign key references cliente(idcliente)
+	idcuenta varchar(120) not null,
+
+	constraint fk_extras foreign key (idcuenta) references usuarios(idusuario)
 )
 	
-go
-create table rol_usuario(
-	id_rol varchar(64),
-	r_nombre varchar(120) default 'USER',
-	r_status int(1) default 0,
-	
-	
-	constraint pk_rol primary key(id_rol)
-)
+
 
